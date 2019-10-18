42bdd30e94fda4a6aa26adcad157978b24ace892
Angela Wen <wena@seas.upenn.edu>
2019-10-18 17:54:42 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Angela Wen on 10/18/19.
+//  Copyright © 2019 Angela Wen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
